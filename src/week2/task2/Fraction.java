c7b25c803801f3d1d3bba1ab178f625bb96f3c26
hai nd <duchaiuet@gmail.com>
2018-09-27 09:52:52 +0700

    week2

--- src/week2/task2/Fraction.java
+++ src/week2/task2/Fraction.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-public class Fraction{
+class Fraction{
 	protected int numerator, denominator ;
 	Fraction(){};
 	Fraction (int a, int b)
@@ -38,9 +38,9 @@ public class Fraction{
 		}
 		return b;
 	}
-	fraction  multiplication(fraction a)
+	Fraction  multiply(Fraction a)
 	{
-		fraction b = new fraction();
+		Fraction b = new Fraction();
 		b.numerator = this.numerator * a.numerator;
 		b.denominator = this.denominator * a.denominator;
 		return b;		
@@ -55,19 +55,19 @@ public class Fraction{
 		}
 		else System.out.println("EROR")	;
 	}
-	fraction division (fraction a)
+	Fraction divide(Fraction a)
 	{
 		if ( a.denominator !=0 && this.denominator != 0 ) {
-		fraction b = new fraction();
+		Fraction b = new Fraction();
 		b.numerator = this.numerator * a.denominator;
 		b.denominator = this.denominator * a.numerator;
 		return b;
 		}
 		else return null;
 	}
-	fraction subtraction (fraction a)
+	Fraction subtract (Fraction a)
 	{
-		fraction b = new fraction();
+		Fraction b = new Fraction();
 		if ( this.denominator == a.denominator) {
 			b.numerator = this.numerator - a.numerator;
 			b.denominator = this.denominator;
@@ -91,49 +91,13 @@ public class Fraction{
 			System.out.println(this.numerator+"/"+this.denominator);
 		}
 	}
-	public boolean equals(fraction s)
+	public boolean equals(Object s)
 	{
-		this.shortten();
-		s.shortten();
-		if(this.numerator == s.numerator && this.denominator == s.denominator) return true;
-		else return false;
-		
+		Fraction other = (Fraction) s;
+		Fraction a = this.subtract(other);
+		if( a.numerator == 0) return true;
+		return false;
+					
 	}
 
-}
-
-public class Task2 {
-	static Scanner sc = new Scanner ( System.in);
-	public static void main ( String []args)
-	{	
-		fraction a = new fraction ();
-		fraction b = new fraction ();
-		fraction c = new fraction ();
-		System.out.print("Enter the Numerator: ");
-		a.numerator = sc.nextInt();
-		System.out.print("Enter the Denominator: ");
-		a.denominator = sc.nextInt();
-		System.out.print("Enter the Numerator: ");
-		b.numerator = sc.nextInt();
-		System.out.print("Enter the Denominator: ");
-		b.denominator = sc.nextInt();
-		c = a.add(b);
-		c.shortten();
-		System.out.print("Tong cua hai phan so la: ");	
-		c.display();
-		c = a.multiplication(b);
-		c.shortten();
-		System.out.print("tich cua hai phan so la: ");	
-		c.display();
-		c = a.division(b);
-		c.shortten();
-		System.out.print("Thuong cua hai phan so la: ");	
-		c.display();
-		c = a.subtraction(b);
-		//c.shortten();
-		System.out.print("Hieu cua hai phan so la: ");	
-		c.display();
-		if( a.equals(b)) System.out.print("hai phan so giong nhau.");
-		else System.out.print("hai phan so khac nhau.");
-	}
 }
