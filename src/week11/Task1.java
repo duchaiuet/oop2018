689920d79bf034a1d53ee17ed99e9535f1124d2b
hai nd <duchaiuet@gmail.com>
2018-11-22 16:38:39 +0700

    week 11
    bài tập tuần 11

--- src/week11/Task1.java
+++ src/week11/Task1.java
@@ -1,4 +1,61 @@
 package week11;
 
 public class Task1 {
+	public static <T extends Comparable<T>> void sort(T []a){
+		if(a.length == 0 ) return;
+		else {
+			for(int i = 0; i < a.length -1 ;i++) {
+				for(int j = 0; j < a.length -1 ; j++ )
+				{
+					if(a[j].compareTo(a[j+1]) < 0) {
+						T gan = a[j];
+						a[j] = a[j+1];
+						a[j+1] = gan;
+					}
+						
+				}
+			}
+		}
+	}
+	public static <T> void print(T []a) {
+		for(int i = 0; i < a.length;i++)
+		{
+			System.out.print(a[i]+" ");
+		}
+	}
+	public static void main(String[] args) {
+		Character a[] = {'a','g','h','d','q','k','t','n'};
+		sort(a);
+		print(a);
+		System.out.println();
+		Integer b[] = {5,6,1,2,4,3,2,8,9,4,0};
+		sort(b);
+		print(b);
+		System.out.println();
+		Long c[] = {123l,234l,7456l,5634l,433l,768l,75l,78l};
+		sort(c);
+		print(c);
+		System.out.println();
+		Byte d[] = {4,5,8,6,2,4,1,9};
+		sort(d);
+		print(d);
+		System.out.println();
+		Short e[] = {22,29,25,23,28,27,12};
+		sort(e);
+		print(e);
+		System.out.println();
+		Float f[] = {6.1f,1.2f,2.8f,1.9f,7.9f};
+		sort(f);
+		print(f);
+		System.out.println();		
+		Double  g[] = {3.245124,8.62314,9.21345,1.2451};
+		sort(g);
+		print(g);
+		System.out.println();
+	}
 }
+	
+	
+	
+	
+
--- src/week11/Task2.java
+++ src/week11/Task2.java
@@ -1,4 +1,50 @@
 package week11;
 
+import java.util.ArrayList;
+
 public class Task2 {
+	public static <T extends Comparable<T>> T Max( ArrayList<T> a){
+			T max = a.get(0);
+			for(int i = 0; i < a.size() ; i++) {
+				if(a.get(i).compareTo(max) > 0) {
+					max = a.get(i);
+				}
+			}
+			return max;
+	}
+	public static void main(String[] args) {
+		ArrayList<Integer> _Int = new ArrayList<>();
+		_Int.add(3);
+		_Int.add(2);
+		_Int.add(6);
+		_Int.add(7);
+		_Int.add(8);
+		_Int.add(9);
+		System.out.println(Max(_Int));
+		ArrayList<Character> _Character = new ArrayList<>();
+		_Character.add('a');
+		_Character.add('b');
+		_Character.add('t');
+		_Character.add('p');
+		_Character.add('h');
+		_Character.add('u');
+		System.out.println(Max(_Character));
+		ArrayList<Long> _Long = new ArrayList<>();
+		_Long.add(34l);
+		_Long.add(28l);
+		_Long.add(69l);
+		_Long.add(47l);
+		_Long.add(68l);
+		_Long.add(9l);
+		System.out.println(Max(_Long));
+		ArrayList<Double> _Double = new ArrayList<>();
+		_Double.add(3.1245);
+		_Double.add(2.6785);
+		_Double.add(6.2535);
+		_Double.add(7.7845);
+		_Double.add(8.124);
+		_Double.add(9.0001);
+		System.out.println(Max(_Double));
+
+	}
 }
